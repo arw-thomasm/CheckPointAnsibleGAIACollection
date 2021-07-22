4134c4c95eb770457c3c11062ebc2174df4fd096
thomas <thomas@die-markos.at>
2021-07-22 08:46:45 +0200

    added documentation

--- plugins/modules/cp_gaia_first_time_wizard.py
+++ plugins/modules/cp_gaia_first_time_wizard.py
@@ -31,30 +31,97 @@ from ansible_collections.check_point.gaia.plugins.module_utils.checkpoint import
 DOCUMENTATION = """
 author: Thomas Marko (@arw-thomasm)
 description:
-- Running the first time wizard to create a Smartcenter/MDM server, a gateway or a cluster node
+- Run the first time wizard to create a Smartcenter/MDM server, gateway or cluster node
 module: cp_gaia_first_time_wizard
 options:
+  password:
+    description: password for user admin
+    type: str
+    required: True
+    
+  security_management:
+    description: Install Security Management or Multi domain server
+    type: dict
+    options:
+      multi_domain:
+        description: Install Security Multi domain server, it can be primary or secondary or log-server according to type parameter
+        type: bool
+        required: True
+      type:
+        description: type of security management or multi domain server
+        type: str
+        required: True
+        choices: 'primary'|'secondary'|'log-server'
+      activation_key:
+        description: Secure Internal Communication key, relevant in case of secondary or log-server
+        type: str
+        required: False
+      gui_clients:
+        description: choose which GUI clients can log into the Security Management. fill one of the parameters (range/network/single-ip), for multi-domain it can be only single-ip or can keep the default value (any)
+        type: dict
+        options: 
+          range:
+            description: range of IPs allowed to connect to management
+            type: dict
+            options:
+              first_IPv4_range:
+                description: first ipv4-address
+                type: str
+              last_IPv4_range:
+                description: last ipv4-address
+                type: str
+          network:
+            description: IPs from specific network allowed to connect to management
+            type: dict
+            options:
+              ip_network_address:
+                description: ipv4 network address
+                type: str
+              IPv4_masklen:
+                description: masklength (valid values are 1-32)
+                type: int
+          single_ip:
+            description: In case of a single IP which allowed to connect to management
+            type: str
+        leading_interface:
+          description: leading multi domain server interface, relevant in case of multi-domain enabled
+          type: str
+  security_gateway:
+    description: Install Security Gateway
+    type: dict
+    options:
+      dynamically_assigned_ip:
+        description: Enable DAIP (dynamic ip) gateway. Should be false if cluster-member or security-management enabled
+        type: bool
+      activation_key:
+        description: Secure Internal Communication key
+        type: str
+        required: True
+      cluster_member:
+        description: Enable/Disable ClusterXL
+        type: bool
 
-short_description: Run the First Time Wizard
+short_description: Run the First Time Wizard for Management
 version_added: '2.9'
 """
 
 EXAMPLES = """
-- name: Setup a primary Smartcenter Server
-  cp_gaia_first_time_wizard:
-    password: vpn123
-    security_management:
-      multi_domain: False
-      type: primary
-      gui_clients:
-        network:
-          ip_network_address: 192.168.0.0
-          IPv4_masklen: 24
+- name: create a primary smartcenter server
+      cp_gaia_first_time_wizard:
+        password: vpn123
+        security_management:
+          multi_domain: False
+          type: primary
+          gui_clients:
+            network:
+              ip_network_address: 192.168.0.0
+              IPv4_masklen: 24
+      register: result
 """
 
 RETURN = """
 task_id:
-  description: The task-id of the asynchronous task
+  description: The task-id of the asynchronous task of the FTW
   returned: always
   type: str
 """
@@ -62,9 +129,9 @@ task_id:
 
 def main():
     # arguments for the module:
-    fields = dict(
+    argument_spec = dict(
+      password=dict(type='str', required=True, no_log=True),
       security_management=dict(type='dict', options=dict(
-        password=dict(type='str', required=True),
         multi_domain=dict(type='bool', required=True),
         type=dict(type='str', required=True, choices=['primary', 'secondary', 'log-server']),
         activation_key=dict(type='str', required=False),
@@ -87,7 +154,7 @@ def main():
         cluster_member=dict(type='bool')
       ))
     )
-    module = AnsibleModule(argument_spec=fields, supports_check_mode=True)
+    module = AnsibleModule(argument_spec=argument_spec, supports_check_mode=True)
     api_call_object = 'set-initial-setup'
 
     res = api_call(module, api_call_object)
